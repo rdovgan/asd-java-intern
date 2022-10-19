e981133791072e0c3b920f5a03fb8b49307aee29
Roman Demian <demurelebowski@gmail.com>
2022-10-19 10:11:02 +0300

    added class ConverterService wich implements IsConverterService

---
+++ src/main/java/team/asd/service/ConverterService.java
@@ -0,0 +1,39 @@
+package team.asd.service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class ConverterService implements IsConverterService {
+
+    @Override
+    public String convertIntegerIntoString(Integer value) {
+        if (value == null) {
+            return null;
+        }
+        return value.toString();
+    }
+
+    @Override
+    public Integer convertStringIntoInteger(String value) {
+        if (value == null) {
+            return null;
+        }
+        return Integer.valueOf(value);
+    }
+
+    @Override
+    public Double convertStringIntoDouble(String value) {
+        if (value == null) {
+            return null;
+        }
+        return Double.valueOf(value);
+    }
+
+    @Override
+    public LocalDate convertStringIntoLocalDate(String dateString) throws DateTimeParseException {
+        if (dateString == null) {
+            return null;
+        }
+        return LocalDate.parse(dateString);
+    }
+}
