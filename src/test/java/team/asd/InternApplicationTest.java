6f18346c648f90b4787fabe18d879fc7e29102be
Olena Syrotenko <syrotenko.career@gmail.com>
2022-11-01 14:09:27 +0200

    #22 Configured Spring Boot for project

---
+++ src/test/java/team/asd/InternApplicationTest.java
@@ -0,0 +1,13 @@
+package team.asd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
