e47acb15054ebdd9f01c318ab39695e9a4138bc5
Olena Syrotenko <syrotenko.career@gmail.com>
2022-11-02 13:31:34 +0200

    #23 Implemented TestMessageController

---
+++ src/main/java/team/asd/controller/TestMessageController.java
@@ -0,0 +1,15 @@
+package team.asd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.asd.entity.TestMessage;
+
+import java.time.LocalDate;
+
+@RestController
+public class TestMessageController {
+	@GetMapping("/test/message")
+	public TestMessage sendTestMessage() {
+		return new TestMessage(LocalDate.now(), "Test message from Spring Boot Application!");
+	}
+}
